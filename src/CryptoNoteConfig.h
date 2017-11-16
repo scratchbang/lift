252209e19fb85dd84a2e235f2c329800e9454aac
scratchbang <33736344+scratchbang@users.noreply.github.com>
2017-11-17 00:58:08 +0200

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -1,1 +1,6 @@
 const char CRYPTONOTE_NAME[] = "liftcoin";
+const uint64_t MONEY_SUPPLY = (uint64_t)(-1);
+const unsigned EMISSION_SPEED_FACTOR = 18;
+const uint64_t DIFFICULTY_TARGET = 99;
+const int P2P_DEFAULT_PORT = 1535;
+const int RPC_DEFAULT_PORT = 3231;
